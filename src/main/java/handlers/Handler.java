b8c8bab36d76f78271c9d5e3f1b6bd1725025988
MariaaAaA89 <msemkina.mar@mail.ru>
2025-05-28 12:04:31 +0300

    2
    2

--- src/main/java/handlers/Handler.java
+++ src/main/java/handlers/Handler.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-interface Handler {
+public interface Handler {
     void setNextHandler(Handler handler);
     List<Monster> handle(File request) throws IOException;
 }
